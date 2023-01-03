057a7f933b4ec9d2b70fc3379b2f1a2934ed8119
gchu79 <69404476+gchu79@users.noreply.github.com>
2023-01-02 23:50:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -40,6 +40,11 @@ document.querySelectorAll('.bible-grid [data-book]').forEach(item => {
 
 document.querySelectorAll('.version-grid [data-version]').forEach(item => {
     item.addEventListener('click', event => {
+		if (item.innerHTML === 'GRK-HEB') {
+			if (bible.outputNumberTextElement.innerHTML === '') {bible.outputNumberTextElement.innerHTML = '1'};
+            window.open("https://biblehub.com/interlinear/" + bible.outputBookTextElement.innerHTML.replace('Song of Solomon','Songs').replace(' ','_').toLowerCase() + "/" + bible.outputNumberTextElement.innerHTML + ".htm");            
+        }
+		if (item.innerHTML.includes("GRK")) return
         if (bible.outputVersionTextElement.innerHTML.includes(item.innerHTML)) return
         if (bible.outputVersionTextElement.innerHTML.split(";").length > 3 ) return
         if (bible.outputVersionTextElement.innerHTML === "") {
@@ -53,10 +58,28 @@ document.querySelectorAll('.version-grid [data-version]').forEach(item => {
 
 document.querySelectorAll('.number-grid [data-number]').forEach(item => {
     item.addEventListener('click', event => {
+        if ((bible.outputNumberTextElement.innerHTML === "") & (item.innerHTML === "0")) return
         bible.outputNumberTextElement.innerHTML += item.innerHTML
         if (parseInt(bible.outputNumberTextElement.innerHTML) > document.querySelector('[data-max]').innerHTML) {
             bible.outputNumberTextElement.innerHTML = document.querySelector('[data-max]').innerHTML;
         }
+    })
+})
+
+document.querySelectorAll('.number-grid [operation-number]').forEach(item => {
+    item.addEventListener('click', event => {
+
+        if ((item.innerHTML === "-") && ( parseInt(bible.outputNumberTextElement.innerHTML) === 1)) return
+        if ((item.innerHTML === "+") && ( parseInt(bible.outputNumberTextElement.innerHTML) >= document.querySelector('[data-max]').innerHTML )) return
+
+        if (item.innerHTML === "+") {
+            if (bible.outputNumberTextElement.innerHTML === "") {bible.outputNumberTextElement.innerHTML = "0"}
+            bible.outputNumberTextElement.innerHTML = parseInt(bible.outputNumberTextElement.innerHTML) + 1;
+        }
+        if (item.innerHTML === "-") {
+            if (bible.outputNumberTextElement.innerHTML === "") {bible.outputNumberTextElement.innerHTML = document.querySelector('[data-max]').innerHTML}
+            bible.outputNumberTextElement.innerHTML = parseInt(bible.outputNumberTextElement.innerHTML) - 1;
+        }        
     })
 })
 
@@ -69,14 +92,22 @@ document.querySelectorAll('.output-grid [select-option]').forEach(item => {
         if (item.innerHTML === 'BibleHub Parallel') {
             window.open("https://biblehub.com/" + bible.outputBookTextElement.innerHTML.replace('Song of Solomon','Songs').replace(' ','_').toLowerCase() + "/" + bible.outputNumberTextElement.innerHTML + ".htm");        
         }
-        if (item.innerHTML === 'BibleGateway') {
+        if (item.innerHTML === 'Bible Gateway') {
             window.open("https://www.biblegateway.com/passage/?search=" + bible.outputBookTextElement.innerHTML.toLowerCase() + "%20" + bible.outputNumberTextElement.innerHTML + "&version=" + bible.outputVersionTextElement.innerHTML);        
         }
         if (item.innerHTML === 'BibleHub Interlinear') {
             window.open("https://biblehub.com/interlinear/" + bible.outputBookTextElement.innerHTML.replace('Song of Solomon','Songs').replace(' ','_').toLowerCase() + "/" + bible.outputNumberTextElement.innerHTML + "-1.htm");            
         }
+        if ((item.innerHTML === 'Pawson') && (stuff[window.idx].Constable.substring(0,2) === 'ot')) {
+			console.log(stuff[window.idx].Constable.substring(0,2));
+			window.open("https://www.davidpawson.org/resources/series/unlocking-the-new-testament");
+		}
+        if ((item.innerHTML === 'Pawson') && (stuff[window.idx].Constable.substring(0,2) === 'nt')) {
+			console.log(stuff[window.idx].Constable.substring(0,2));
+			window.open("https://www.davidpawson.org/resources/series/unlocking-the-new-testament-1");
+		}				
         if (item.innerHTML === 'Swindoll') {
-            console.log(window.idx);
+			console.log(window.idx);
             window.open("https://insight.org/resources/bible/" + stuff[window.idx].Swindoll);            
         }
          if (item.innerHTML === 'Stedman') {
@@ -92,10 +123,11 @@ document.querySelectorAll('.output-grid [select-option]').forEach(item => {
             window.book = bible.outputBookTextElement.innerHTML
             window.book = window.book.replace('Genesis','Genesis-1-11').replace('Exodus','Exodus-1-18').replace('1 Kings','1-2-Kings').replace('2 Kings','1-2-Kings').replace('1 Chronicles','1-2-Chronicles').replace('2 Chronicles','1-2-Chronicles').replace('Song of Solomon','Song-of-Songs').replace('Ezra','Ezra-Nehemih').replace('Nehemiah','Ezra-Nehemiah').replace('Nehemih','Nehemiah').replace('1 John','1-3-John').replace('2 John','1-3-John').replace('3 John','1-3-John').replace(' ','-')  
             window.open("https://www.bibleproject.com/explore/" + window.book);            
-        }        
+        } 
         
         
-         if (item.innerHTML === 'BlueLetterBible') {
+        
+         if (item.innerHTML === 'Blue Letter Bible') {
             console.log(window.idx);
             window.open("https://www.blueletterbible.org");            
         }        
@@ -103,6 +135,17 @@ document.querySelectorAll('.output-grid [select-option]').forEach(item => {
             console.log(window.idx);
             window.open("https://bible.org/");
         }
+         if (item.innerHTML === 'Mounce Greek') {
+            console.log(window.idx);
+            window.open("https://www.billmounce.com/greek-dictionary");
+        }
+
+         if (item.innerHTML === 'Truth for Life') {
+            console.log(window.idx);
+            window.open("https://truthforlife.org/resources/?type=sermon&scripture=" + bible.outputBookTextElement.innerHTML);
+        }
+        
+        
     })
 })
 
